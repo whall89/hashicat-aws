ead7aab45b746f5044179a02e3af327054af0e97
William Hall <hallwilliam98@gmail.com>
2021-02-10 14:14:50 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. We do cat pics, memes and cheezburgerz.
+  Welcome to ${PREFIX}'s app. We do cat pics, memes and cheezburgerzzz.
   <!-- END -->
 
   </div>
